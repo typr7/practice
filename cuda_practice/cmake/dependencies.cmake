db74679056d1ddcf7cd8dcde365e154766be62bc
typr7 <blame773@gmail.com>
2026-02-07 01:49:19 +0800

    Update cuda_practice

--- cuda_practice/cmake/dependencies.cmake
+++ cuda_practice/cmake/dependencies.cmake
@@ -1,38 +1,14 @@
+# stb_image
+
 include(FetchContent)
 
 FetchContent_Declare(
-    opencv
-    GIT_REPOSITORY  https://github.com/opencv/opencv.git
-    GIT_TAG         4.13.0
-    GIT_SHALLOW     TRUE
+    stb
+    GIT_REPOSITORY https://github.com/nothings/stb.git
+    GIT_TAG master
 )
 
-set(BUILD_LIST "core,imgcodecs,imgproc" CACHE STRING "" FORCE)
+FetchContent_MakeAvailable(stb)
 
-set(BUILD_SHARED_LIBS        OFF CACHE BOOL "" FORCE)
-set(BUILD_TESTS              OFF CACHE BOOL "" FORCE)
-set(BUILD_PERF_TESTS         OFF CACHE BOOL "" FORCE)
-set(BUILD_EXAMPLES           OFF CACHE BOOL "" FORCE)
-set(BUILD_opencv_apps        OFF CACHE BOOL "" FORCE)
-set(BUILD_opencv_python3     OFF CACHE BOOL "" FORCE)
-set(BUILD_JAVA               OFF CACHE BOOL "" FORCE)
-set(BUILD_opencv_js          OFF CACHE BOOL "" FORCE)
-
-set(WITH_GTK                 OFF CACHE BOOL "" FORCE)
-set(WITH_QT                  OFF CACHE BOOL "" FORCE)
-set(WITH_FFMPEG              OFF CACHE BOOL "" FORCE)
-set(WITH_GSTREAMER           OFF CACHE BOOL "" FORCE)
-set(WITH_V4L                 OFF CACHE BOOL "" FORCE)
-set(WITH_1394                OFF CACHE BOOL "" FORCE)
-set(WITH_CUDA                OFF CACHE BOOL "" FORCE)
-set(WITH_OPENCL              OFF CACHE BOOL "" FORCE)
-set(WITH_IPP                 OFF CACHE BOOL "" FORCE)
-set(WITH_PROTOBUF            OFF CACHE BOOL "" FORCE)
-
-set(WITH_PNG                 ON  CACHE BOOL "" FORCE)
-set(WITH_JPEG                ON  CACHE BOOL "" FORCE)
-set(WITH_TIFF                OFF CACHE BOOL "" FORCE)
-set(WITH_WEBP                OFF CACHE BOOL "" FORCE)
-set(WITH_OPENJPEG            OFF CACHE BOOL "" FORCE)
-
-FetchContent_MakeAvailable(opencv)
+add_library(stb_image INTERFACE)
+target_include_directories(stb_image INTERFACE ${stb_SOURCE_DIR})
--- cuda_practice/src/02_rgb_to_gray/rgb_to_gray.cu
+++ cuda_practice/src/02_rgb_to_gray/rgb_to_gray.cu
@@ -1,4 +1,91 @@
+#include <cstdio>
+#include <cstddef>
+#include <cstdlib>
+#include <algorithm>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb_image_write.h>
+
+
+__device__ float
+clamp(const float val, const float lo, const float hi)
+{
+    return fmaxf(fminf(val, hi), lo);
+}
+
+__global__ void
+imageRGBToGray(const uint8_t*   input,
+               const int        width,
+               const int        height,
+               uint8_t*         output)
+{
+    const int px = blockDim.x * blockIdx.x + threadIdx.x;
+    const int py = blockDim.y * blockIdx.y + threadIdx.y;
+    if (px < width && py < height) {
+        const int pi = (width * py + px) * 3;
+        const uint8_t gray = static_cast<uint8_t>(
+            clamp(
+                0.21f * input[pi] + 0.72f * input[pi + 1] + 0.07f * input[pi + 2],
+                0.f,
+                255.f
+            )
+        );
+        output[width * py + px] = gray;
+    }
+}
+
 int main()
 {
+    int width, height, channels;
+    const uint8_t* data = stbi_load(
+        "images.jpg",
+        &width,
+        &height,
+        &channels,
+        0
+    );
+
+    if (data) {
+        printf(
+            "width: %d, height: %d, channels: %d\n",
+            width,
+            height,
+            channels
+        );
+    } else {
+        printf("read image failed\n");
+    }
+
+    uint8_t* data_d = nullptr;
+    cudaMalloc(&data_d, sizeof(uint8_t) * width * height * 3);
+    uint8_t* res_d = nullptr;
+    cudaMalloc(&res_d, sizeof(uint8_t) * width * height);
+
+    cudaMemcpy(data_d, data, sizeof(uint8_t) * width * height * 3, cudaMemcpyHostToDevice);
+
+    const dim3 grid_dim(std::ceil(width / 16.0), std::ceil(height / 16.0), 1);
+    const dim3 block_dim(16, 16, 1);
+
+    imageRGBToGray<<<grid_dim, block_dim>>>(data_d, width, height, res_d);
+    cudaDeviceSynchronize();
+
+    uint8_t* res = static_cast<uint8_t*>(malloc(sizeof(uint8_t) * width * height));
+    cudaMemcpy(res, res_d, sizeof(uint8_t) * width * height, cudaMemcpyDeviceToHost);
+
+    const int ok = stbi_write_jpg("output.jpg", width, height, 1, res, width);
+
+    if (!ok) {
+        printf("save processed image failed\n");
+    }
+
+    cudaFree(data_d);
+    cudaFree(res_d);
+
+    stbi_image_free(const_cast<uint8_t*>(data));
+    free(res);
+
     return 0;
 }
